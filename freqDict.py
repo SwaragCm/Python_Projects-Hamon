95885f5bdfb6a482b514b9fbe13d6c96e8a4ff82
SwaragCm <swaragcm@gmail.com>
2024-05-21 23:02:51 +0530

    Freq Detailed -Implemented Python function to calculate character frequencies in a string

--- freqDict.py
+++ freqDict.py
@@ -1,10 +1,10 @@
 def freq(string):
     dictionary = {}
-    for i in string:
-        if i in dictionary:         
-            dictionary[i] += 1
+    for i in string:                # iterates over each character in the string
+        if i in dictionary:         # checks current character i is already a key in the dictionary.
+            dictionary[i] += 1      # if it is their corresponding value is incremented by 1.
         else:
-            dictionary[i] = 1
+            dictionary[i] = 1       # if not their it sets the value 1
     print(dictionary)
 
 if __name__ == "__main__":
