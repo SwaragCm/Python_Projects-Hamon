1673fd9552d91f01aa6c7b1ef476ea9135dcd34b
SwaragCm <swaragcm@gmail.com>
2024-05-21 16:34:13 +0530

    Pangram -Created a function to check whether the sentence is pangram or not

--- panagram.py
+++ panagram.py
@@ -1,10 +1,7 @@
-import string
-
 def panagram(s):
-    alpha = "abcdefghijklmnopqrstuvwxyz"  #here we are initializing the alphabets to "alpha" variable with the help of "string" which we import above
-    print(alpha)
+    alpha = "abcdefghijklmnopqrstuvwxyz"     # here we are initializing the alphabets to "alpha"
     for i in alpha:
-        if i not in s.lower():          #this will convert string variable to lowercase
+        if i not in s.lower():               # "lower()" this will convert string to lowercase
             print("not panagram")
             break
     else:
@@ -13,5 +10,5 @@ def panagram(s):
 
 
 if __name__=="__main__":
-    phrase=str(input("Enter a phrase"))
+    phrase = str(input("Enter a phrase"))
     panagram(phrase)
