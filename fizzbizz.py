b1237031ec93bcd47b797814900bc383a217cbcb
SwaragCm <swaragcm@gmail.com>
2024-05-21 11:45:34 +0530

    fizzbizz

--- fizzbizz.py
+++ fizzbizz.py
@@ -14,20 +14,3 @@ if __name__=="__main__":
     n=int(input("Enter a number"))
     fizzbizz(n)
 
-
-
-# def fizzbizz(n):
-#     for i in range(1,n+1):
-#         if i % 3 ==0:
-#             print("fizz")
-#         elif i % 5 ==0:
-#             print("bizz")
-#         elif i % 15 ==0:
-#             print("fizzbizz")
-#         else:
-#             print(i)
-
-
-# if __name__=="__main__":
-#     n=int(input("Enter a number"))
-#     fizzbizz(n)
